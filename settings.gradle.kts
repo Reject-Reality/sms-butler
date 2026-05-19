d0736bec2efe295db1d8597586a735d5a70de457
Reject-Reality <294215988@qq.com>
2026-05-19 23:40:44 +0800

    revert: remove mirrors, use official Gradle/Maven sources

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,5 @@
 pluginManagement {
     repositories {
-        // 国内镜像源（阿里云），加速依赖下载
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google/") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
-        // 官方源作为备选
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -13,8 +8,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google/") }
         google()
         mavenCentral()
     }
