2ace4faf009c9de7d88a78081688016a6675be74
Reject-Reality <294215988@qq.com>
2026-05-19 22:59:16 +0800

    chore: add Aliyun Maven mirrors + Tencent Gradle mirror for faster downloads in China

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 pluginManagement {
     repositories {
+        // 国内镜像源（阿里云），加速依赖下载
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+        // 官方源作为备选
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -8,6 +13,8 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
         google()
         mavenCentral()
     }
